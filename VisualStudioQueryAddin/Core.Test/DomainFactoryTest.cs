6abbf3f431d9cae3067c80a4789f6a28363d0211
sankalp saxena <sankalpsaxena81@gmail.com>
2011-09-02 12:56:46 -0600

    <SS> First Commit

---
+++ VisualStudioQueryAddin/Core.Test/DomainFactoryTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Core.Test
+{
+    [TestFixture]
+    public class DomainFactoryTest
+    {
+    }
+}
